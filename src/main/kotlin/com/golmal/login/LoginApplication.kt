e032e14c2f72254b235b08cc92fa4839625cf0d3
sushant patil <sushant.patil@lendico.de>
2021-01-01 15:46:18 +0100

    Initial Commit

---
+++ src/main/kotlin/com/golmal/login/LoginApplication.kt
@@ -0,0 +1,11 @@
+package com.golmal.login
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoginApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoginApplication>(*args)
+}
